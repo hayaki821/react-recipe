c0d171049f4668ccbb048b7f9a627fcdf128d49b
hayaki kento <khkur821@i.softbank.jp>
2022-06-25 17:48:53 +0900

    swagger-ui-express

--- nx/first-org/libs/ui/src/lib/todos/todos.tsx
+++ nx/first-org/libs/ui/src/lib/todos/todos.tsx
@@ -19,7 +19,7 @@ export function Todos(props: TodosProps) {
             key={`todos-${t.id}`}
             className={'todo'}
           >
-            {t.title}!!
+            {t.title}
           </li>
         ))}
       </ul>
