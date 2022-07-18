ceb12c518a2820475a8e55c34f162e8a10e15ba9
hayaki kento <khkur821@i.softbank.jp>
2022-07-18 14:31:24 +0900

    完成

--- news-app/news-app/src/components/pickup-article/index.tsx
+++ news-app/news-app/src/components/pickup-article/index.tsx
@@ -1,35 +1,33 @@
 import styles from "./index.module.scss";
 import moment from "moment";
 import Props from "../types";
-import Image from "next/image";
 
-const Article: React.FC<Props> = ({ articles, title }) => {
+const PickupArticle: React.FC<Props> = ({ articles }) => {
   return (
-    <section className={styles.article}>
-      <div className={styles.article__heading}>
-        <h1>{title.charAt(0).toUpperCase() + title.slice(1).toLowerCase()}</h1>
-      </div>
+    <section className={styles.pickup}>
+      <h1 className={styles.article__heading}>PickUp</h1>
       {articles.map((article, index) => {
         const time = moment(article.publishedAt || moment.now())
-          .fromNow()
-          .slice(0, 1);
+            .fromNow()
+            .slice(0, 1) == "a"
+            ? 1
+            : moment(article.publishedAt || moment.now())
+                .fromNow()
+                .slice(0, 1);
         return (
           <a href={article.url} key={index} target="_blank" rel="noopener noreferrer">
             <article className={styles.article__main}>
               <div className={styles.article__title}>
                 <p>{article.title}</p>
                 <p className={styles.article__time}>
-                  {time}
-                  時間前
+                  {time}時間前
                 </p>
               </div>
               {article.urlToImage && (
-                // eslint-disable-next-line @next/next/no-img-element
                 <img
                   key={index}
                   src={article.urlToImage}
                   className={styles.article__img}
-                  alt={`${article.title} image`}
                 />
               )}
             </article>
@@ -40,4 +38,4 @@ const Article: React.FC<Props> = ({ articles, title }) => {
   );
 };
 
-export default Article;
+export default PickupArticle;
